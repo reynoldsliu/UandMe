a7bf6693f7dc453dd53fab7a8581e0054ccd9b4b
reynolds <reynoldsliu01443@gmail.com>
2023-07-28 16:44:02 +0800

    reynolds branch update push

--- src/main/java/Reynolds.java
+++ src/main/java/Reynolds.java
@@ -2,6 +2,6 @@
 public class Reynolds {
 	public static void main(String[] args) {
 		System.out.println("大家放心 William會把我們扛起來的!!");
-		System.out.println("扛起來");
+		System.out.println("扛起來!!!!!!!!!!!!!!!!!!!!!");
 	}
 }
