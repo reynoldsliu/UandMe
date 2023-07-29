0b5f70505882597c3709ebcf4c1acbabedf9f7b2
reynolds <reynoldsliu01443@gmail.com>
2023-07-29 14:56:30 +0800

    no message

--- src/main/java/Jas.java
+++ src/main/java/Jas.java
@@ -1,10 +1,10 @@
 
 public class Jas {
-public static void main(String[] args) {
+	public static void main(String[] args) {
 
-	System.out.println("啥意思?????");
+		System.out.println("啥意思?????");
 
-	System.out.println("行不行呢_test2");
+		System.out.println("行不行呢_test2");
 
-}
+	}
 }
