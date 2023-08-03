5b367b467aa27f5837733b7ea9c618a89212a923
reynolds <reynoldsliu01443@gmail.com>
2023-08-03 12:33:38 +0800

    test right

--- src/main/java/Reynolds.java
+++ src/main/java/Reynolds.java
@@ -2,7 +2,7 @@
 public class Reynolds {
 	public static void main(String[] args) {
 		System.out.println("大家放心 William會把我們扛起來的!!");
-		System.out.println("扛起來!!!!!!!!!!!!!!!!!!!!!")
+		System.out.println("扛起來!!!!!!!!!!!!!!!!!!!!!");
 		System.out.println("testing for comp  iler check!");
 	}
 }
